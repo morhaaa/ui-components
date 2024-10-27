a2d2265cd1f5102f1c5ee5ffd8d25606a2a70e90
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-10-27 16:41:11 +0100

    chore: add tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import "./tailwind.css";
+import { withThemeByClassName } from "@storybook/addon-themes";
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +11,16 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
